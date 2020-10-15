2bfaf1db4e1cd3b11f88ebd288b0292a33f41032
Katada10 <katadafreije@gmail.com>
2020-10-15 09:51:09 -0400

    Finished Image loading without camera/device

--- app/src/main/java/com/example/lab4/MainActivity.java
+++ app/src/main/java/com/example/lab4/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +32,40 @@ public class MainActivity extends AppCompatActivity {
 //Application Context and Activity
         Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
         startActivityForResult (intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_CANCELED) return;
+//Getting the Avatar Image we show to our users
+        ImageView avatarImage = (ImageView) findViewById(R.id.logoImage);
+//Figuring out the correct image
+        String drawableName = "ic_logo_00";
+        switch (data.getIntExtra("imageID", R.id.teamid00)) {
+            case R.id.teamid00:
+                drawableName = "ic_logo_00";
+                break;
+            case R.id.teamid01:
+                drawableName = "ic_logo_01";
+                break;
+            case R.id.teamid02:
+                drawableName = "ic_logo_02";
+                break;
+            case R.id.teamid03:
+                drawableName = "ic_logo_03";
+                break;
+            case R.id.teamid04:
+                drawableName = "ic_logo_04";
+                break;
+            case R.id.teamid05:
+                drawableName = "ic_logo_05";
+                break;
+            default:
+                drawableName = "ic_logo_00";
+                break;
+        }
+        int resID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+        avatarImage.setImageResource(resID);
     }
 
 }
